1a9447f0ec213a4f1efd6a180efb0810750d0276
Tester Siladi <testersiladi@gmail.com>
2021-09-17 15:59:44 +0200

    Loader added

---
+++ quotes.js
@@ -0,0 +1,12 @@
+const localQuotes = [
+    {text: 'The only thing to do with good advice is to pass it on. It is never of any use to oneself.', author: 'Oscar Wilde'},
+    {text: 'Life is not measured by the breaths we take, but by the moments that take our breath.', author: null},
+    {text: 'The smallest flower is a thought, a life answering… Whole, of whom they have a persistent intuition.', author: 'Honore de Balzac'},
+    {text: 'Consider how hard it is to change yourself and you…ittle chance you have in trying to change others.', author: 'Jacob Braude'},
+    {text: "If you'll not settle for anything less than your b… amazed at what you can accomplish in your lives.", author: 'Vince Lombardi'},
+    {text: 'What lies behind us and what lies before us are small matters compared to what lies within us.', author: 'Oliver Holmes'},
+    {text: 'With the realization of ones own potential and sel…ce in ones ability, one can build a better world.', author: 'Dalai Lama'},
+    {text: 'There is nothing like returning to a place that re…find the ways in which you yourself have altered.', author: 'Nelson Mandela'},
+    {text: 'Forget about all the reasons why something may not…ou only need to find one good reason why it will.', author: 'Robert Anthony'},
+    {text: 'It is the mark of an educated mind to be able to entertain a thought without accepting it.', author: 'Aristotle'}
+];
